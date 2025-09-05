d96d7e1e4adc732cc487cb22aa02af538b8ac0da
ElMoiz22 <aelzaazaa22@gmail.com>
2025-09-05 01:57:09 -0600

    .

--- fotossupergallos/crearMiniaturas.js
+++ fotossupergallos/crearMiniaturas.js
@@ -12,7 +12,7 @@ if (!fs.existsSync(output)) fs.mkdirSync(output, { recursive: true });
 fs.readdirSync(folder).forEach(file => {
   if (file.endsWith(".png")) {
     sharp(path.join(folder, file))
-      .resize({ width: 300 }) // ancho máximo de la miniatura
+      .resize({ width: 150 }) // ancho máximo de la miniatura
       .toFile(path.join(output, file.replace(".png", ".png")))
       .then(() => console.log(`Miniatura creada: ${file}`))
       .catch(err => console.error(err));
