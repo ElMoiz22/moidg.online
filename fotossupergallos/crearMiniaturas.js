aff1dc7d6b1cced975422013042404355b38ff56
ElMoiz22 <aelzaazaa22@gmail.com>
2025-09-05 01:45:58 -0600

    Fotos Equipo San Pedro - Plantel Mayor

---
+++ fotossupergallos/crearMiniaturas.js
@@ -0,0 +1,20 @@
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+
+const folder = "./equipomayor/";       // Carpeta donde están tus PNG originales
+const output = "./equipomayor/thumbs/"; // Carpeta donde se guardarán las miniaturas
+
+// Crear carpeta de miniaturas si no existe
+if (!fs.existsSync(output)) fs.mkdirSync(output, { recursive: true });
+
+// Leer todos los archivos de la carpeta
+fs.readdirSync(folder).forEach(file => {
+  if (file.endsWith(".png")) {
+    sharp(path.join(folder, file))
+      .resize({ width: 300 }) // ancho máximo de la miniatura
+      .toFile(path.join(output, file.replace(".png", ".png")))
+      .then(() => console.log(`Miniatura creada: ${file}`))
+      .catch(err => console.error(err));
+  }
+});
