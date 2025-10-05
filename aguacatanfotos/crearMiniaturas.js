1f539c07911ba7f75d520f33ed7af896a186c9e0
ElMoiz22 <aelzaazaa22@gmail.com>
2025-10-04 21:10:33 -0600

    aguacatanfotos

---
+++ aguacatanfotos/crearMiniaturas.js
@@ -0,0 +1,20 @@
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+
+const folder = "./especialesj9/";       // Carpeta donde están tus PNG originales
+const output = "./especialesj9/thumbs/"; // Carpeta donde se guardarán las miniaturas
+
+// Crear carpeta de miniaturas si no existe
+if (!fs.existsSync(output)) fs.mkdirSync(output, { recursive: true });
+
+// Leer todos los archivos de la carpeta
+fs.readdirSync(folder).forEach(file => {
+  if (file.endsWith(".png")) {
+    sharp(path.join(folder, file))
+      .resize({ width: 150 }) // ancho máximo de la miniatura
+      .toFile(path.join(output, file.replace(".png", ".png")))
+      .then(() => console.log(`Miniatura creada: ${file}`))
+      .catch(err => console.error(err));
+  }
+});
