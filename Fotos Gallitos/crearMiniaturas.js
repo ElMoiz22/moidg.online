79ca3fa1cf4af2c11a6ea161a1eeebac3ae85276
ElMoiz22 <aelzaazaa22@gmail.com>
2025-10-06 13:09:30 -0600

    Fotos Categorías Especiales

--- Fotos Gallitos/crearMiniaturas.js
+++ Fotos Gallitos/crearMiniaturas.js
@@ -2,8 +2,8 @@ const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
 
-const folder = "./especial/";       // Carpeta donde están tus PNG originales
-const output = "./especial/thumbs/"; // Carpeta donde se guardarán las miniaturas
+const folder = "./especialj9/";       // Carpeta donde están tus PNG originales
+const output = "./especialj9/thumbs/"; // Carpeta donde se guardarán las miniaturas
 
 // Crear carpeta de miniaturas si no existe
 if (!fs.existsSync(output)) fs.mkdirSync(output, { recursive: true });
